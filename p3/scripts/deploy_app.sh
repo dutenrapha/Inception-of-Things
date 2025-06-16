5cefcc60ac3275ee2e6d6bc6ba0ffa213bd52556
angelasoler <manzanaembicicleta@gmail.com>
2025-06-16 20:56:24 -0300

    adjust p3

--- p3/scripts/deploy_app.sh
+++ p3/scripts/deploy_app.sh
@@ -2,6 +2,6 @@
 set -e
 
 echo "Deploying the Argo CD Application for the playground app..."
-kubectl apply -f config/argocd.yaml -n argo-cd
+kubectl apply -f ../config/argocd.yaml -n argo-cd
 
 echo "Application deployed. Argo CD will now monitor the GitHub repository for changes."
