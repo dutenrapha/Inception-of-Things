2ccf1219a47170bbec82265344f2463b99c17a3d
angelasoler <manzanaembicicleta@gmail.com>
2025-06-16 23:59:47 -0300

    p3 running ok

--- p3/scripts/deploy_argocd.sh
+++ p3/scripts/deploy_argocd.sh
@@ -8,3 +8,17 @@ echo "Waiting for the Argo CD server to be ready..."
 kubectl rollout status deployment/argocd-server -n argo-cd
 
 echo "Argo CD deployed successfully."
+
+kubectl port-forward svc/argocd-server -n argo-cd 8080:443 > /dev/null 2>&1 &
+PORT_FORWARD_PID=$!
+
+# Save the PID to a file for later use
+echo $PORT_FORWARD_PID > /tmp/argocd_port_forward.pid
+
+# Retrieve the Argo CD initial admin password
+PASSWORD=$(kubectl -n argo-cd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+
+# Print a user-friendly message
+echo "Argo CD server is now accessible at http://localhost:8080"
+echo "Your initial admin password is: $PASSWORD"
+echo "To stop the port-forwarding, use: kill $(cat /tmp/argocd_port_forward.pid)"
